d50ecc75db51dbf7aa53fb067eea1b3843b9c5b4
Nikita Zolotoverkh <example@gmail.com>
2023-07-26 00:02:51 +0300

    all functionality wrapped into functions, some additional functions done

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-'use strict'
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""),
+let numberOfFilms,
     counter = 0,
     movieName,
     movieScore;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
+    }
+}
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,6 +18,50 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+function questioningLoop() {
+    for (counter; counter < personalMovieDB.count; counter++) {
+    movieName = prompt('Укажите название фильма', '');
+    if (movieName === null || movieName.length > 15 || movieName === '') {
+        counter--
+        continue
+    }
+    movieScore = +prompt('Сколько баллов вы бы поставили ему?', '');
+    if (movieScore === null || movieScore > 10 || movieScore === 0) {
+        counter--
+        continue
+    }
+    personalMovieDB.movies[movieName] = movieScore;
+    }
+}
+
+questioningLoop();
+
+function response() {
+    if (personalMovieDB.count < 10) {
+        console.log('Ты посмотрел недостаточно фильмов');
+    } else if (10 <= personalMovieDB.count && personalMovieDB.count <= 30) {
+        console.log('Неплохо');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Ты очень часто смотришь фильмы, у тебя что других дел нет?');
+    } else {
+        console.log('error')
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Какой ваш любимый жанр под номером ${i + 1}`)
+    }
+}
+writeYourGenres();
+
+function showMyDB() {
+    if (!personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB();
 
 // while (counter < personalMovieDB.count) {
 //     movieName = prompt('Укажите название фильма', '');
@@ -40,29 +92,3 @@ const personalMovieDB = {
 //     personalMovieDB.movies[movieName] = movieScore;
 //     counter++;  
 // } while (counter < personalMovieDB.count)
-
-for (counter; counter < personalMovieDB.count; counter++) {
-    movieName = prompt('Укажите название фильма', '');
-    if (movieName === null || movieName.length > 15 || movieName === '') {
-        counter--
-        continue
-    }
-    movieScore = +prompt('Сколько баллов вы бы поставили ему?', '');
-    if (movieScore === null || movieScore > 10 || movieScore === 0) {
-        counter--
-        continue
-    }
-    personalMovieDB.movies[movieName] = movieScore;
-}
-
-if (personalMovieDB.count < 10) {
-    console.log('Ты посмотрел недостаточно фильмов');
-} else if (10 <= personalMovieDB.count && personalMovieDB.count <= 30) {
-    console.log('Неплохо');
-} else if (personalMovieDB.count > 30) {
-    console.log('Ты очень часто смотришь фильмы, у тебя что других дел нет?');
-} else {
-    console.log('error')
-}
-
-console.log(personalMovieDB);
