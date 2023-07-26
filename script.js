d312524953e7c7b6911ad3c4fc2a1d1c2c84e928
Nikita Zolotoverkh <example@gmail.com>
2023-07-26 23:36:34 +0300

    all function refactored as main object methods, some new methods added

--- script.js
+++ script.js
@@ -1,67 +1,76 @@
-let numberOfFilms,
-    counter = 0,
+let counter = 0,
     movieName,
     movieScore;
 
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
-    }
-}
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function start() {
+        this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+        while (this.count == '' || this.count == null || isNaN(this.count)) {
+            this.count = +prompt("Сколько фильмов вы уже посмотрели?", "")
+        }
+    },
+    questioningLoop: function() {
+        for (counter; counter < personalMovieDB.count; counter++) {
+        movieName = prompt('Укажите название фильма', '');
+        if (movieName === null || movieName.length > 15 || movieName === '') {
+            counter--
+            continue
+        }
+        movieScore = +prompt('Сколько баллов вы бы поставили ему?', '');
+        if (movieScore === null || movieScore > 10 || movieScore === 0) {
+            counter--
+            continue
+        }
+        personalMovieDB.movies[movieName] = movieScore;
+        }
+    },
+    response: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Ты посмотрел недостаточно фильмов');
+        } else if (10 <= personalMovieDB.count && personalMovieDB.count <= 30) {
+            console.log('Неплохо');
+        } else if (personalMovieDB.count > 30) {
+            console.log('Ты очень часто смотришь фильмы, у тебя что других дел нет?');
+        } else {
+            console.log('error')
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 0; i < 3; i++) {
+            personalMovieDB.genres[i] = prompt(`Какой ваш любимый жанр под номером ${i + 1}`);
+            if(personalMovieDB.genres[i] === null || personalMovieDB.genres[i] === '') {
+                i--;
+                continue;
+            }
+        }
+        personalMovieDB.genres.forEach(function(item, i) {
+            console.log(`Любимый жанр #${++i} - это ${item}`);
+        });
+    },
+    showMyDB: function () {
+        if (!personalMovieDB.privat) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if(this.privat) {
+            this.privat = false;
+        } else {
+            this.privat = true;
+        }
+    }
 };
 
-function questioningLoop() {
-    for (counter; counter < personalMovieDB.count; counter++) {
-    movieName = prompt('Укажите название фильма', '');
-    if (movieName === null || movieName.length > 15 || movieName === '') {
-        counter--
-        continue
-    }
-    movieScore = +prompt('Сколько баллов вы бы поставили ему?', '');
-    if (movieScore === null || movieScore > 10 || movieScore === 0) {
-        counter--
-        continue
-    }
-    personalMovieDB.movies[movieName] = movieScore;
-    }
-}
-
-questioningLoop();
-
-function response() {
-    if (personalMovieDB.count < 10) {
-        console.log('Ты посмотрел недостаточно фильмов');
-    } else if (10 <= personalMovieDB.count && personalMovieDB.count <= 30) {
-        console.log('Неплохо');
-    } else if (personalMovieDB.count > 30) {
-        console.log('Ты очень часто смотришь фильмы, у тебя что других дел нет?');
-    } else {
-        console.log('error')
-    }
-}
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`Какой ваш любимый жанр под номером ${i + 1}`)
-    }
-}
-writeYourGenres();
-
-function showMyDB() {
-    if (!personalMovieDB.privat) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB();
+personalMovieDB.start();
+personalMovieDB.questioningLoop();
+personalMovieDB.response();
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyDB();
 
 // while (counter < personalMovieDB.count) {
 //     movieName = prompt('Укажите название фильма', '');
